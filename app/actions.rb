474b445992c39e5760d1e116973d8406a63f5be8
tristan <tsmackinlay@gmail.com>
2016-06-13 17:47:23 -0700

    added track submission and page navigation

--- app/actions.rb
+++ app/actions.rb
@@ -1,9 +1,28 @@
 # Homepage (Root path)
 get '/' do
-  erb :index
+  erb :'tracks/index'
 end
 
 get '/tracks' do
   @tracks = Track.all
   erb :'tracks/index'
+end
+
+get '/tracks/new' do
+  @track = Track.new
+  erb :'tracks/new'
+end
+
+post '/tracks' do
+  @track = Track.new(
+    title: params[:title],
+    author: params[:author],
+    url: params[:url]
+  )
+  if
+    @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'
+  end
 end
